833190e998d2275f486b4464bf649c1d799430f6
Allan Resende <allanresendegomes@outlook.com>
2024-11-25 11:33:05 -0300

    Update margin bottom do h2 de produtos

--- frontend/resources/views/livewire/produtos-table.blade.php
+++ frontend/resources/views/livewire/produtos-table.blade.php
@@ -4,7 +4,7 @@
         Nenhum produto encontrado.
     </div>
     @else
-        <h2 class="px-4 sm:px-6 mb-2 text-gray-800">Produtos</h2>
+        <h2 class="px-4 sm:px-6 mb-4 text-gray-800">Produtos</h2>
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                 <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
