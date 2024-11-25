bba34f37d8ef04309e181217387f7dd11d468724
Allan Resende <allanresendegomes@outlook.com>
2024-11-25 10:43:52 -0300

    Update navigation.blade.php

--- frontend/resources/views/livewire/welcome/navigation.blade.php
+++ frontend/resources/views/livewire/welcome/navigation.blade.php
@@ -9,7 +9,7 @@
     @else
         <a
             href="{{ route('login') }}"
-            class="border border-gray-300 rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+            class="rounded-md px-3 py-2 text-gray-900 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
         >
             Entrar
         </a>
@@ -17,7 +17,7 @@
         @if (Route::has('register'))
             <a
                 href="{{ route('register') }}"
-                class="border border-gray-300 rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
+                class="rounded-md px-3 py-2 text-gray-900 ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20]"
             >
                 Registrar
             </a>
